203b47305bfb1582d1a446e94918caa094a9b038
Dhatraditya J <aditya@digitalchakra.in>
2013-01-24 14:25:41 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="keywords" content="web design, web development">
+	<meta name="tags" content="web design, web development">
+	<title>DIGITAL CHAKRA - IT CONSULTING | WEB DESIGN & DEVELOPMENT</title>
+	<link rel="stylesheet" type="text/css" href="css/stylesheet.css"/>
+</head>
+<body>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script src="jquery.touchslider.min.js"></script>
+	<script src="jquery.nicescroll.js"></script>
+		<script>
+			jQuery(function($) {
+				$(".touchslider").touchSlider({mouseTouch: true, autoplay: true});
+				nice = $("html").niceScroll();
+			});
+		</script>
+    <header id="topHeader">
+		<div class="wrap">
+			<div class="clsLogo">
+				<h1><a href="#" title="DigitalChakra">Digital Chakra</a></h1>
+			</div>
+			<nav class="clsMainNav">
+				<ul class="clsMainNavList">
+					<li><a href="#">The Big picture</a></li>
+					<li><a href="#whatWeDo">What we do</a></li>
+					<li><a href="#dcBlog">DC Blog</a></li>
+					<li><a href="#contact">Contact Us</a></li>
+				</ul>
+			</nav>
+		</div>
+    </header>
+    <div id="bodyContent" class="clsBodyContent">
+		<!-- ===========================================Big picture =========================================================== -->
+		<div id="bigPic" class="clsBigPic">
+			<div class="wrap">
+			
+			<img src="images/dcLogoBig.jpg"/>
+				<div class="clsBigPicContent">
+					<h2 class="largeTitle">Hi !</h2>
+					<h2 class="largeTitle2">We just dropped anchor</h2>
+					</br>
+					<p class="largeContent"> We provide services to a universal audience </br> some of which can be viewed here</p>
+				</div>
+				<div class="spaceShuttle">
+					<img src="images/spaceShuttle.png" />
+				</div>
+			</div>	
+		</div>
+		<!-- ===========================================What we do =========================================================== -->
+		<div id="whatWeDo">		
+				
+			<div class="touchslider">
+				
+				<div class="touchslider-viewport" style="width:985px;overflow:hidden;position:relative;height:600px;margin:0 auto;">
+					<div>
+						<div class="touchslider-item">
+							<div id="carousel1">
+								<h2 class="largeTitle3">Product development</h2></br></br>
+								<p class="contentText">Digital Chakra is helping businesses worldwide, big or small achieve their goals by fulfilling their online needs using open source technology stacks and industry best practices. </p>
+							</div>
+						</div>
+						<div class="touchslider-item">
+							<div id="carousel2">
+								<h2 class="largeTitle3">Custom build web applications</h2></br></br>
+								<p class="contentText">Digital Chakra is helping businesses worldwide, big or small achieve their goals by fulfilling their online needs using open source technology stacks and industry best practices. </p>
+							</div>
+						</div>
+						<div class="touchslider-item">
+							<div id="carousel3">
+								<h2 class="largeTitle3">SAAS based offerings</h2></br></br>
+								<p class="contentText">Digital Chakra is helping businesses worldwide, big or small achieve their goals by fulfilling their online needs using open source technology stacks and industry best practices. </p>
+							</div>
+						</div>
+						<div class="touchslider-item">
+							<div id="carousel4">
+								<h2 class="largeTitle3">Design consulting & offerings</h2></br></br>
+								<p class="contentText">Digital Chakra is helping businesses worldwide, big or small achieve their goals by fulfilling their online needs using open source technology stacks and industry best practices. </p>
+							</div>
+						</div>
+						<div class="touchslider-item">
+							<div id="carousel5">
+								<h2 class="largeTitle3">Online product consulting & development</h2></br></br>
+								<p class="contentText">Digital Chakra is helping businesses worldwide, big or small achieve their goals by fulfilling their online needs using open source technology stacks and industry best practices. </p>
+							</div>
+						</div>
+						<div class="touchslider-item">
+							<div id="carousel6">
+								<h2 class="largeTitle3">Cloud computing</h2>
+								</br></br>
+								<p class="contentText">Digital Chakra is helping businesses worldwide, big or small achieve their goals by fulfilling their online needs using open source technology stacks and industry best practices. </p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div  class="prevNext" >
+					
+					<a class="touchslider-prev leftArrow">Prev</a>
+					
+					
+					<a class="touchslider-next rightArrow">Next</a>
+					
+					
+				</div>
+				<div style="display:none">
+						
+						<a class="touchslider-nav-item touchslider-nav-item-current">1</a>
+						<a class="touchslider-nav-item">2</a>
+						<a class="touchslider-nav-item">3</a>
+						<a class="touchslider-nav-item">4</a>
+						<a class="touchslider-nav-item">5</a>
+						<a class="touchslider-nav-item">6</a>
+						
+				</div>
+					
+			</div>
+				
+		</div>	
+		
+								
+		
+		<!-- ===========================================DC blog =========================================================== -->
+		<div id="dcBlog">
+			<div class="wrap">
+			
+			<div class="dcBlogSection">
+				<ul class="twitterFeed">
+					<li>
+					<div class="listBlock">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a quam ipsum. In ut dapibus quam. Praesent eu ipsum vitae est sagittis facilisis vitae id odio.</p>
+						<p class="listContent2">2 hours ago</p>
+					</div>
+					</li>
+					<li>
+					<div class="listBlock">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a quam ipsum. In ut dapibus quam. Praesent eu ipsum vitae est sagittisfacilisis vitae id odio.</p>
+						<p class="listContent2">2 hours ago</p>
+					</div>
+					</li>
+					
+					<li>
+					<div class="listBlock">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a  quam ipsum. In ut dapibus quam. Praesent eu ipsum vitae est sagittis facilisis vitae id odio.</p>
+						<p class="listContent2">2 hours ago</p>
+					</div>
+					</li>
+					
+					<li>
+					<div class="listBlock">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a  quam ipsum. In ut dapibus quam. Praesent eu ipsum vitae est sagittis facilisis vitae id odio.</p>
+						<p class="listContent2">2 hours ago</p>
+					</div>
+					</li>
+				</ul>
+				<div class="blogTitle1">
+					<p>follow us on twitter</p>
+				</div>
+			</div>
+			<div class="dcBlogSection">
+				<ul class="twitterFeed">
+					<li>
+					<div class="listBlock">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a quam ipsum. In ut dapibus quam. Praesent eu ipsum vitae est sagittis facilisis vitae id odio.</p>
+						<p class="listContent2">2 hours ago</p>
+					</div>
+					</li>
+					<li>
+					<div class="listBlock">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a quam ipsum. In ut dapibus quam. Praesent eu ipsum vitae est sagittisfacilisis vitae id odio.</p>
+						<p class="listContent2">2 hours ago</p>
+					</div>
+					</li>
+					
+					<li>
+					<div class="listBlock">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a  quam ipsum. In ut dapibus quam. Praesent eu ipsum vitae est sagittis facilisis vitae id odio.</p>
+						<p class="listContent2">2 hours ago</p>
+					</div>
+					</li>
+					
+					<li>
+					<div class="listBlock">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a  quam ipsum. In ut dapibus quam. Praesent eu ipsum vitae est sagittis facilisis vitae id odio.</p>
+						<p class="listContent2">2 hours ago</p>
+					</div>
+					</li>
+				</ul>
+				<div class="blogTitle2">
+					<p>Blog</p>
+				</div>
+			</div>
+			</div>
+		</div>
+		<!-- ===========================================Contact us =========================================================== -->
+		<div id="contact">
+			<div class="wrap">
+				<div class="contactSection">
+					<div class="contactSpaceShuttle">
+						<img src="images/spaceShuttle_contacts.png" />
+					</div>
+						<ul class="contactList">
+							<!--<li ><a href="#" class="contactIn">linked in id</a></li>-->
+							<li ><a href="#" class="contactFb">facebook page</a></li>
+							<li ><a href="#" class="contactTr">twitter page</a></li>
+							<li ><a href="#" class="contactEmail">email id</a></li>
+							<li ><a href="#" class="contactHome">Address</a></li>
+						</ul>
+				</div>
+				<div class="contactSection2">
+				<form id="contactForm" method="post" action="">
+					 <ul id="status" class="">  
+						<li id="info">There were some problems with your form submission:</li>  
+					</ul>  
+						
+					<label for="name">Name</label>
+					<input type="text" name="name" placeholder="Full Name">
+					 
+					<label for="email">E-mail</label>
+					<input type="email" name="email" placeholder="name@domain.com">
+					 
+					<label for="skype">Skype id</label>
+					<input type="email" name="email" placeholder="skype id">
+					
+					<label for="phone">Phone</label>
+					<input type="tel" name="phone" placeholder="ex. (555) 555-5555">
+					 
+					<label for="location">Location</label>
+					<input type="url" name="url" placeholder="City or state">
+					
+					<label for="message">What can we help you with</label>	
+					<textarea name="message" placeholder="Your message must be greater than 20 charcters"  ></textarea>  
+					 
+					<input id="submit" type="submit" name="submit" value="Submit" />
+					
+
+				</form>
+
+				 
+				</div>
+			</div>
+		</div>
+    </div>
+   
+    <footer>
+		<section>
+        <p>© copyright 2013 | www.digitalchakra.in </p>
+		</br> 	
+		</section>	
+    </footer>
+</body>
+</html>
