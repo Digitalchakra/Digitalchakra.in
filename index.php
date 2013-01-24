3d26d968b431fcaa6b82c17667956ac56f34c152
Dhatraditya J <aditya@digitalchakra.in>
2013-01-24 17:49:04 +0530

    Updated DC info

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
 	<meta name="keywords" content="web design, web development">
 	<meta name="tags" content="web design, web development">
-	<title>DIGITAL CHAKRA - IT CONSULTING | WEB DESIGN & DEVELOPMENT</title>
+	<meta name="description" content="Business Technology Consulting, IT solutions and IT Services, Design & UI/UX Consulting, Cloud Computing and support services that deliver measurable business value. Transform your business, and make Operations efficient with innovative solutions from Digital Chakra">
+	<title>Digital Chakra – Web Development, Design & Cloud Computing Solutions.</title>
 	<link rel="stylesheet" type="text/css" href="css/stylesheet.css"/>
 </head>
 <body id="body">
@@ -64,7 +65,7 @@
 					<h2 class="largeTitle">Hello World! We've just dropped anchor on earth.</h2>
 					<h2 class="largeTitle2"></h2>
 					</br>
-					<p class="largeContent"> We provide IT services and solutions to a universal audience, </br> some of which can be viewed here</p>
+					<p class="largeContent"> We provide IT services and solutions to a universal audience, </br> as highlighted below</p>
 				</div>
 				<div class="spaceShuttle">
 					<img src="images/spaceShuttle.png" />
@@ -80,26 +81,44 @@
 					<div>
 						<div class="touchslider-item">
 							<div id="carousel1">
-								<h2 class="largeTitle3">Product development</h2></br></br>
-								<p class="contentText">Digital Chakra is helping businesses worldwide, big or small achieve their goals by fulfilling their online needs using open source technology stacks and industry best practices. </p>
+								<h2 class="largeTitle3">Product Development</h2></br></br>
+								<p class="contentText">We form a seamless extension of your team so that you are freed up to concentrate on the core activities of your business. We help you take your bright idea to success backed by strong development processes and best practices. Our services include:
+									<ul style=" margin:16px;" class="contentText">
+										<li>UI Design and Branding Services</li>
+										<li>Product Development/Enchancement</li>
+										<li>QA, Testing and Support</li>
+									</ul></p>
 							</div>
 						</div>
 						<div class="touchslider-item">
 							<div id="carousel2">
-								<h2 class="largeTitle3">Custom build web applications</h2></br></br>
-								<p class="contentText">Digital Chakra is helping businesses worldwide, big or small achieve their goals by fulfilling their online needs using open source technology stacks and industry best practices. </p>
+								<h2 class="largeTitle3">Custom-built Web Applications</h2></br></br>
+								<p class="contentText">We provide end-to-end development services from the initial use case analysis to post-implementation support. Industry standard best practices and latest technologies ensure high quality apps every time.Our customer-centric process ensures that you effectively plan, implement and maintain a solution that is in line with your business objectives.
+ 							<ul style=" margin:16px;" class="contentText">
+										<li>Low Cost, High Quality, On Time</li>
+										<li>Agile, Process Driven Approach</li>
+										<li>Effective Collaboration and Communication</li>
+										<li>Adaptable, Flexible Solutions</li>
+									</ul></p>
 							</div>
 						</div>
 						<div class="touchslider-item">
 							<div id="carousel3">
-								<h2 class="largeTitle3">SAAS based offerings</h2></br></br>
+								<h2 class="largeTitle3">SAAS - based Offerings</h2></br></br>
 								<p class="contentText">Digital Chakra is helping businesses worldwide, big or small achieve their goals by fulfilling their online needs using open source technology stacks and industry best practices. </p>
 							</div>
 						</div>
 						<div class="touchslider-item">
 							<div id="carousel4">
-								<h2 class="largeTitle3">Design consulting & offerings</h2></br></br>
-								<p class="contentText">Digital Chakra is helping businesses worldwide, big or small achieve their goals by fulfilling their online needs using open source technology stacks and industry best practices. </p>
+								<h2 class="largeTitle3">Design Consultation & Offerings</h2></br></br>
+								<p class="contentText">To us design is not just about “what it looks like”. It is also about what or rather how well it does the job. We take your idea and help you convert it into a compelling user experience with our creative team of designers and experienced team of UI/UX consultants and business analysts.
+								<ul style=" margin:16px;" class="contentText">
+										<li>Concept Development, Functional Spec & Analysis</li>
+										<li>UX Focused High fidelity mock-ups</li>
+										<li>Interactive Wireframe Prototyping</li>
+										<li>Cross-Browser Testing, Responsive Design</li>
+										<li>JS Framework Integration & SEO</li>
+									</ul></p>
 							</div>
 						</div>
 						<div class="touchslider-item">
@@ -271,8 +290,8 @@ Ph : +9144 2433-0401  +9144 2433-0402<a href="http://goo.gl/maps/y8n25" target="
    
     <footer>
 		<section>
-        <p>© copyright 2013 | www.digitalchakra.in </p>
-		</br>
+        <p style="margin-bottom:20px;">© copyright 2013 | www.digitalchakra.in </p>
+		
 		</section>	
     </footer>
 </body>
