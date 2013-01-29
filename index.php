8621bf8cd2fd5183cb5b66658ffd81003435ebae
Dhatraditya J <aditya@digitalchakra.in>
2013-01-29 14:07:37 +0530

    fixed w3 validation

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <!DOCTYPE html>
 <html>
-	<head profile="http://www.w3.org/2005/10/profile">
-<link rel="icon"
-      type="image/png"
-      href="images/favicon.png">
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="title" content=" India affordable web development, design and cloud deployment services">
+	<head>
+	<link rel="icon" type="image/png" href="images/favicon.png">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="keywords" content="web development, web design, responsive design, product development, cloud computing">
-	<meta name="tags" content="web design, web development, digitalchakra, digital chakra">
 	<meta name="description" content="Digital Chakra is a leader in Web Design, Development and Deployment based out of Chennai, India. We offer Business Technology, UI/UX Consulting and cloud computing services/support. Improve your business reach and efficiency with responsive web design solutions and mobile applications.">
 	<title>Digital Chakra | Web Development, Design & Cloud Computing Solutions	Made Affordable!</title>
 	<link rel="stylesheet" type="text/css" href="css/dc.min.c788d0f223aeb6bc143908fc5ea34026e531ca79.css"/>
@@ -35,15 +31,15 @@
 		<div id="bigPic" class="clsBigPic">
 			<div class="wrap">
 
-			<!--<img src="images/dcLogoBig.jpg"/> -->
+			<!--<img src="images/dcLogoBig.jpg" alt="logo_digitalchakra_big"/> -->
 				<div class="clsBigPicContent">
 					<h2 class="largeTitle2">Hello World!</h2>
 					<h2 class="largeTitle2">We've just dropped anchor on earth.</h2>
-					</br>
-					<p class="largeContent">Digital Chakra is professional web development and offshore outsourcing company based in Chennai, India. We provide web design, web development and deployment services, product development and cloud based application development. Take your business to the next level with mobile-friendly responsive web designs and mobile applications.<a class="anchor" style="color:#47c8f5; text-decoration: none;"href="#whatWeDo"> Learn More&nbsp;<img src="images/triangle_small.png"/></a></p>
+					<br />
+					<p class="largeContent">Digital Chakra is professional web development and offshore outsourcing company based in Chennai, India. We provide web design, web development and deployment services, product development and cloud based application development. Take your business to the next level with mobile-friendly responsive web designs and mobile applications.<a class="anchor" style="color:#47c8f5; text-decoration: none;" href="#whatWeDo"> Learn More&nbsp;<img src="images/triangle_small.png" alt="Learn More"/></a></p>
 				</div>
 				<div class="spaceShuttle">
-					<img src="images/spaceShuttle.png" />
+					<img src="images/spaceShuttle.png" alt="Space Shuttle" />
 				</div>
 			</div>
 		</div>
@@ -56,62 +52,62 @@
 					<div>
 						<div class="touchslider-item">
 							<div id="carousel1">
-								<h2 class="largeTitle3">Product Development</h2></br></br>
-								<p class="contentText">We form a seamless extension of your team so that you are freed up to concentrate on the core activities of your business. We help you take your bright idea to success backed by strong development processes and best practices. Our services include:
+								<h2 class="largeTitle3">Product Development</h2><br/><br/>
+								<p class="contentText">We form a seamless extension of your team so that you are freed up to concentrate on the core activities of your business. We help you take your bright idea to success backed by strong development processes and best practices. Our services include:</p>
 									<ul style=" margin:16px;" class="contentText">
 										<li>UI Design and Branding Services</li>
 										<li>Product Development/Enchancement</li>
 										<li>QA, Testing and Support</li>
-									</ul></p>
+									</ul>
 							</div>
 						</div>
 						<div class="touchslider-item">
 							<div id="carousel2">
-								<h2 class="largeTitle3">Custom-built Web Applications</h2></br></br>
-								<p class="contentText">We provide end-to-end development services from the initial use case analysis to post-implementation support. Industry standard best practices and latest technologies ensure high quality apps every time.Our customer-centric process ensures that you effectively plan, implement and maintain a solution that is in line with your business objectives.
+								<h2 class="largeTitle3">Custom-built Web Applications</h2><br/><br/>
+								<p class="contentText">We provide end-to-end development services from the initial use case analysis to post-implementation support. Industry standard best practices and latest technologies ensure high quality apps every time.Our customer-centric process ensures that you effectively plan, implement and maintain a solution that is in line with your business objectives.</p>
  							<ul style=" margin:16px;" class="contentText">
 										<li>Low Cost, High Quality, On Time</li>
 										<li>Agile, Process Driven Approach</li>
 										<li>Effective Collaboration and Communication</li>
 										<li>Adaptable, Flexible Solutions</li>
-									</ul></p>
+									</ul>
 							</div>
 						</div>
 						<div class="touchslider-item">
 							<div id="carousel4">
-								<h2 class="largeTitle3">Design Consultation & Offerings</h2></br></br>
-								<p class="contentText">To us design is not just about what it looks like. It is also about what or rather how well it does the job. We take your idea and help you convert it into a compelling user experience with our creative team of designers and experienced team of UI/UX consultants and business analysts.
+								<h2 class="largeTitle3">Design Consultation & Offerings</h2><br/><br/>
+								<p class="contentText">To us design is not just about what it looks like. It is also about what or rather how well it does the job. We take your idea and help you convert it into a compelling user experience with our creative team of designers and experienced team of UI/UX consultants and business analysts.</p>
 								<ul style=" margin:16px;" class="contentText">
 										<li>Concept Development, Functional Spec & Analysis</li>
 										<li>UX Focused High fidelity mock-ups</li>
 										<li>Interactive Wireframe Prototyping</li>
 										<li>Cross-Browser Testing, Responsive Design</li>
 										<li>JS Framework Integration & SEO</li>
-									</ul></p>
+									</ul>
 							</div>
 						</div>
 						<div class="touchslider-item">
 							<div id="carousel5">
-								<h2 class="largeTitle3">Business & Technology Consulting</h2></br></br>
-								<p class="contentText">There are a plethora of tools and software used in the market nowadays for development. So which one should you use to develop your application? What factors should you consider while designing your application? How do you ensure best quality of product or service to your end clients? We help you in:
+								<h2 class="largeTitle3">Business & Technology Consulting</h2><br/><br/>
+								<p class="contentText">There are a plethora of tools and software used in the market nowadays for development. So which one should you use to develop your application? What factors should you consider while designing your application? How do you ensure best quality of product or service to your end clients? We help you in:</p>
 								<ul style=" margin:16px;" class="contentText">
 										<li>Understanding the Industry</li>
 										<li>Quality Assurance</li>
 										<li>Tailored Development Services</li>
-									</ul></p>
+									</ul>
 
 							</div>
 						</div>
 						<div class="touchslider-item">
 							<div id="carousel6">
 								<h2 class="largeTitle3">Cloud Computing</h2>
-								</br></br>
-								<p class="contentText">Taking you to the cloud. Whether you are looking to improve/leverage your existing cloud investment or trying to get there we can do it for you. We have a dedicated group of system admins who can deploy, support and maintain your application on the cloud.
+								<br/><br/>
+								<p class="contentText">Taking you to the cloud. Whether you are looking to improve/leverage your existing cloud investment or trying to get there we can do it for you. We have a dedicated group of system admins who can deploy, support and maintain your application on the cloud.</p>
 								<ul style=" margin:16px;" class="contentText">
 										<li>Cloud Migration Advisory</li>
 										<li>Cloud Application Development</li>
 										<li>Cloud Application Support & Maintenance</li>
-									</ul></p>
+									</ul>
 							</div>
 						</div>
 					</div>
@@ -222,15 +218,15 @@
 			<div class="wrap">
 				<div class="contactSection">
 					<div class="contactSpaceShuttle">
-						<img src="images/spaceShuttle_contacts.png" />
+						<img src="images/spaceShuttle_contacts.png" alt="Contacts" />
 					</div>
 						<ul class="contactList" style="text-align:left;">
 							<!--<li ><a href="#" class="contactIn">linked in id</a></li>-->
-							<li ><a href="https://www.facebook.com/digitalchakra" target="_blank" class="contactFb"style="color:#47c8f5; text-decoration: none;" ><b>Facebook Page</b></a></li>
+							<li ><a href="https://www.facebook.com/digitalchakra" target="_blank" class="contactFb" style="color:#47c8f5; text-decoration: none;" ><b>Facebook Page</b></a></li>
 							<li ><a href="http://twitter.com/digitalchakra" target="_blank" class="contactTr" style="color:#47c8f5; text-decoration: none;" ><b>Twitter Page</b></a></li>
 							<li ><a href="mailto:queries@digitalchakra.in" class="contactEmail" style="color:#47c8f5; text-decoration: none;"><b>queries@digitalchakra.in</b></a></li>
-							<li><p class ="contactHome"><b>Address:</b> Shree Park 3rd Floor West Wing</br> # 578 Anna Salai Chennai-600 018</br>
-Ph : +9144 2433-0401  +9144 2433-0402<a href="http://goo.gl/maps/y8n25" target="_blank"style="color:#47c8f5; text-decoration: none;"><b>Show On Map</b></a></p></li>
+							<li><p class ="contactHome"><b>Address:</b> Shree Park 3rd Floor West Wing<br/> # 578 Anna Salai Chennai-600 018<br/>
+Ph : +9144 2433-0401  +9144 2433-0402<a href="http://goo.gl/maps/y8n25" target="_blank" style="color:#47c8f5; text-decoration: none;"><b>Show On Map</b></a></p></li>
 						</ul>
 				</div>
 				<div class="contactSection2">
