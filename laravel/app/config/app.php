9f6eba4e87f83b1cc7362ba07f62498362357831
Manikandan R <manikandan@digitalchakra.in>
2014-01-27 15:24:35 +0530

    new key generated

--- laravel/app/config/app.php
+++ laravel/app/config/app.php
@@ -65,7 +65,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => 'x3lgzHITxL6mqYUnjUMqzccVmBCVyZq7',
 
 	/*
 	|--------------------------------------------------------------------------
