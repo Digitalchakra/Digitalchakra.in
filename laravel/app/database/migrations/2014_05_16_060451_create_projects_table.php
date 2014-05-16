8955751fb829f1615c87d6035d55e6f84a51308e
Manikandan R <manikandan@digitalchakra.in>
2014-05-16 11:51:16 +0530

    table shema added

---
+++ laravel/app/database/migrations/2014_05_16_060451_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('projects', function($table)
+		{
+			$table->increments('id');
+			$table->string('title',100);
+			$table->text('images');
+			$table->text('description');
+			$table->string('url',200);
+			$table->integer('order')->nullable();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('projects');
+	}
+
+}
